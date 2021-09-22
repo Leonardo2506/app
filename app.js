5e5388d8342c95a1200d2672ea203aab5795b5ba
Leonardo2506 <maggical2014.lf@gmail.com>
2021-09-22 12:26:26 -0300

    leonardo2506

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use('/', require ('./endpoints/index.endpoint'))
 app.set('views', './views')
 app.set('view engine' , 'ejs')
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.listen(port, () => {
     console.log(`App running on ${port}`)
 })
